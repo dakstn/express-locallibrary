b026dcb4c6e33d5016879db4877aa458280c9228
David Stein <dstein@troygroup.com>
2017-08-17 11:04:18 -0400

    Express Tutorial Part 6: Working with forms (completed genre create form).

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var expressValidator = require('express-validator');
 
 // route modules
 var index = require('./routes/index');
@@ -37,6 +38,7 @@ app.set('view engine', 'pug');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(expressValidator()); // this module uses body parser middleware
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
